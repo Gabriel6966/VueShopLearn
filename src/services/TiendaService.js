a18bfd484d153471412d23f1598c7c22969a0f4b
Gabriel6966 <gabrielteren221@gmail.com>
2026-03-20 20:09:24 +0100

    Add cart store & public opinions

--- src/services/TiendaService.js
+++ src/services/TiendaService.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios'
 
 const apiClient = axios.create({
-    //La url que llamamos
-    baseURL: 'http://localhost:3000',
-    //Autenticacion y configuracion para comunicarnos con nuestro server
-    withCredentials: false,
-    headers:{
-        Accept:'application/json',
-        'Content-Type':'application/json',
-    },
+  //La url que llamamos
+  baseURL: 'http://localhost:3000',
+  //Autenticacion y configuracion para comunicarnos con nuestro server
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 })
 
-export default{
-    getCalcetines(id){
-        if(id){
-            return apiClient.get('calcetines/' +id)
-        }
-        return apiClient.get('/calcetines')
+export default {
+  getCalcetines(id) {
+    if (id) {
+      return apiClient.get('calcetines/' + id)
     }
+    return apiClient.get('/calcetines')
+  },
 }
--- src/views/NetworkError.vue
+++ src/views/NetworkError.vue
@@ -1,7 +1,10 @@
 <template>
-  <div style="text-align: center; margin-top: 50px;">
-    <h1 style="font-size: 80px; color: #ff4a4a;">Vaya lo siento</h1>
+  <div style="text-align: center; margin-top: 50px">
+    <h1 style="font-size: 80px; color: #ff4a4a">Vaya lo siento</h1>
     <h2>Parece ser que estamos teniendo unos problemas de conexion interna de red</h2>
-    <h3>Danos un suspiro que en nada lo tenemos, <a href="#" @click.prevent="$router.go(-1)">pulsa aqui</a> para recargar</h3>
+    <h3>
+      Danos un suspiro que en nada lo tenemos,
+      <a href="#" @click.prevent="$router.go(-1)">pulsa aqui</a> para recargar
+    </h3>
   </div>
 </template>
