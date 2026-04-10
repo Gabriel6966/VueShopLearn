1ae087c0d63f09ed3c9cb5a1d310e120b409cffa
Gabriel6966 <gabrielteren221@gmail.com>
2026-04-10 09:49:19 +0200

    Logo & PWA acces

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/styles.css'
-import { datos } from './stores/Usuario'
 
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia)
+app.use(createPinia())
 app.use(router)
 
-//mediante el metodo de recuperar(conectada) recuperamos la sesion al montar la aopp
-const log = datos()
-log.conectada()
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,40 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate', //Actualizacion cuando cambias codigo
+      includeAssets: ['favicon.ico', 'apple-touch-icon-png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Progreso Tienda Gabi',
+        short_name: 'VueShop',
+        description: 'Tienda practic Vue3',
+        theme_color: '#16c0b0',
+        icons: [
+          {
+            src: 'icono.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icono512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  //server: {
+  //  host: true,
+  //  port: 5173,
+  //  strictPort: true,
+  //},
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
